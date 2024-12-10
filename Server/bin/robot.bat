c32a3fea772aac9686050a51c01690a52ba7e953
chensong <15850774503@163.com>
2024-12-10 22:23:31 +0800

    【d】

--- Server/bin/robot.bat
+++ Server/bin/robot.bat
@@ -9,6 +9,6 @@ if "%1" NEQ "" set para=%1
 set Dir=%~dp0%para%\
 
 @ping -n 1 127.0.0.1>nul
-start "Robot" /D %Dir% %Dir%log_collector_client.exe  192.168.1.175  8000  1
+start "Robot" /D %Dir% %Dir%log_collector_client.exe  127.0.0.1  8000  1
 
 REM pause
--- Server/common/build_version.h
+++ Server/common/build_version.h
@@ -1,4 +1,4 @@
 static const char * BUILD_GIT_BRANCH_NAME = "master" ;
-static const int BUILD_GIT_REVERSION = 22   ;
-static const char * BUILD_GIT_HASH = "2153190625b8c48c836a56dd34b0b25b04703767";   ;
-static const char * BUILD_TIME = "5/11-20-4/ 10:45:48";   
+static const int BUILD_GIT_REVERSION = 23   ;
+static const char * BUILD_GIT_HASH = "e7a0903e3c9a83cc9ef13f5733a820529ce67c64";   ;
+static const char * BUILD_TIME = "5/11-20-4/ 12:16:21";   
